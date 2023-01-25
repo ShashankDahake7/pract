6277c363d71a79f5b3b674c0fc01ebfaab78cf54
ShashankDahake7 <119002734+ShashankDahake7@users.noreply.github.com>
2023-01-26 02:38:16 +0530

    Update cpARRAYsPW.c

--- cpARRAYsPW.c
+++ cpARRAYsPW.c
@@ -982,3 +982,56 @@
 //     }
 //     return 0;
 // }
+
+// #include <stdio.h>
+// void spiralPrint(int m, int n, int a[m][n])
+// {
+//     int i, k = 0, l = 0;
+//     /* k - starting row index 
+//         m - ending row index 
+//         l - starting column index 
+//         n - ending column index 
+//         i - iterator */
+//     while (k < m && l < n) {
+//         /* Print the first row from the remaining rows */
+//         for (i = l; i < n; ++i) {
+//             printf("%d ", a[k][i]);
+//         }
+//         k++;
+//         /* Print the last column from the remaining columns */
+//         for (i = k; i < m; ++i) {
+//             printf("%d ", a[i][n - 1]);
+//         }
+//         n--;
+//         /* Print the last row from the remaining rows */
+//         if (k < m) {
+//             for (i = n - 1; i >= l; --i) {
+//                 printf("%d ", a[m - 1][i]);
+//             }
+//             m--;
+//         }
+//         /* Print the first column from the remaining columns */
+//         if (l < n) {
+//             for (i = m - 1; i >= k; --i) {
+//                 printf("%d ", a[i][l]);
+//             }
+//             l++;
+//         }
+//     }
+// }
+// int main()
+// {
+//     int n;
+//     printf("Enter the value of n: ");
+//     scanf("%d", &n);
+//     int a[n][n];
+//     int count = 1;
+//     for (int i = 0; i < n; i++) {
+//         for (int j = 0; j < n; j++) {
+//             a[i][j] = count++;
+//         }
+//     }
+//     printf("\nThe matrix in spiral form is: \n");
+//     spiralPrint(n, n, a);
+//     return 0;
+// }
